2ad3fedf943d5bc19e8cd80aa751e886d2e9bbd1
Abraham Herrera <abrahamhs.mlm@gmail.com>
2025-02-13 21:17:59 +0100

    Refactor DiscoverProvider to load videos on initialization and improve loading logic

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastruture/models/local_video_model.dart';
+
+import '../../shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+//TODO Repositorio y DataSources
+
   bool initialLoading = true;
   List<VideoPost>  videos = [];
 
   Future<void> loadNextPage() async {
-    // TODO: implementar Cargar Videos
-  
+    
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = 
+    videoPosts.map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
 
-
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
   
