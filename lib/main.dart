c12f56a78b2c203470f030cf5c8bbcbf20f3e452
Abraham Herrera <abrahamhs.mlm@gmail.com>
2025-02-16 15:18:06 +0100

    Integrate local video data source and repository into DiscoverProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastruture/datasources/local_video_data_source_impl.dart';
+import 'package:toktik/infrastruture/repositories/video_post_repository_impl.dart';
 
 import 'presentation/providers/discover_provider.dart';
 import 'presentation/screens/discover/discover_screen.dart';
@@ -12,11 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final videoPostRepository = VideoPostRepositoryImpl(
+      videoPostDataSource: LocalVideoDataSourceImpl()
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider( 
           lazy: false,
-          create: (_) => DiscoverProvider()
+          create: (_) => DiscoverProvider(videoPostRepository: videoPostRepository )
           ..loadNextPage()),
         
       ],
